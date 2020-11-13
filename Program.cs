ae465d27d512fb8ce59920b903df1d50018aa2ec
sanketAPatel <68878199+sanketAPatel@users.noreply.github.com>
2020-11-12 19:50:35 -0500

    second question second time

--- Program.cs
+++ Program.cs
@@ -10,17 +10,22 @@ namespace ConsoleApp1
             Console.WriteLine("Enter a  number n:");
             int n = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter a  number:n1");
-            int n1= Convert.ToInt32(Console.ReadLine());
+            int n1 = Convert.ToInt32(Console.ReadLine());
 
-           
-           // no proper spacing 
-           // too many extra lines
-           int a= method1(n,n1);
 
-            Console.WriteLine(a);
+            // no proper spacing 
+            // too many extra lines
+          //  int a = method1(n, n1);
+
+         //   Console.WriteLine(a);
+
+            bool b = method2(n, n1);
+            Console.WriteLine(b);
+
 
         }
-        public  static int method1(int n,int n1) {
+        public static int method1(int n, int n1)
+        {
 
             // If and else should be consistent. Either use {} in both or don't.
             if (n == n1)
