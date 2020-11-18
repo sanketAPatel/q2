a274895e58f68b2b86d663cb9925cdef6a5dc5a6
sanketAPatel <68878199+sanketAPatel@users.noreply.github.com>
2020-11-18 18:53:34 -0500

    fifth question cooment updated

--- Program.cs
+++ Program.cs
@@ -13,37 +13,64 @@ namespace ConsoleApp1
             // Console.WriteLine("Enter a  number:n1");
             // //int n1 = Convert.ToInt32(Console.ReadLine()); */
 
-            Console.WriteLine("Enter a  String:");
+            //Console.WriteLine("Enter a  String:");
             String s1 = Console.ReadLine();
-            Console.WriteLine("Enter a  position you want to remove:n1");
-            int n1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter a  position you want to remove:n1");
+            //int n1 = Convert.ToInt32(Console.ReadLine());
 
-            ///* no proper spacing 
+            //* no proper spacing 
             // too many extra lines
             // int a = method1(n, n1);
             // Console.WriteLine(a);
             // bool b = method2(n, n1);
             // Console.WriteLine(b);
             // Console.WriteLine(Method3(s1));*/
+            // Console.WriteLine(Method4(s1, n1));
 
-            Console.WriteLine(Method4(s1, n1));
+            Method5(s1);
 
 
-        }
-        public static int method1(int n, int n1)
-        {
+            Console.WriteLine("Enter how many array elements you want:");
+            int[] a = null;
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the left shift you want in an array :");
+            int y = Convert.ToInt32(Console.ReadLine());
 
-            // If and else should be consistent. Either use {} in both or don't.
-            if (n == n1)
+            for (int i = 0; i <= n; i++)
             {
-                return 3 * (n + n1);
+                Console.WriteLine(a[i]);
+            }
+           // method6(a[n], y);
+        }
+
+
+
+
+
+
+        public static void Method5(String s1)
+        {
+            int len = s1.Length;
+            Console.WriteLine(s1[len - 1] + s1.Substring(1,len - 2) + s1[0]);
+        }
+
+        public static String Method4(String s1, int n1)
+        {
+            String s2 = (s1.Remove(n1));
+            return s2;
+        }
+        public static String Method3(String s1)
+        {
+            if (s1.StartsWith("if"))
+            {
+                return s1;
             }
             else
-                return (n + n1);
-
+            {
+                string s2 = string.Concat("if", s1);
+                return s2;
+            }
         }
-
-
         public static Boolean method2(int n, int n1)
         {
             if ((n == 30 || n1 == 30) || (n + n1) == 30)
@@ -55,28 +82,22 @@ namespace ConsoleApp1
                 return false;
             }
         }
-
-
-        public static String Method3(String s1)
+        public static int method1(int n, int n1)
         {
-            if (s1.StartsWith("if"))
+
+            // If and else should be consistent. Either use {} in both or don't.
+            if (n == n1)
             {
-                return s1;
+                return 3 * (n + n1);
             }
             else
             {
-
-                string s2 = string.Concat("if", s1);
-                return s2;
+                return (n + n1);
             }
         }
 
 
-        public static String Method4(String s1, int n1)
-        {
 
-            String s2 = (s1.Remove(n1));
-            return s2;
-        }
+
     }
 }
